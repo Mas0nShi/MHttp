3267ea21140209a61df3e20bacbe2b04016cf2bb
Mason <1281814306@qq.com>
2021-07-06 09:57:36 +0800

    add SetProxy, README.md and go.mod

--- test/main.go
+++ test/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"../../MHttp"
-	"github.com/Mas0nShi/goConsole/console"
-	"time"
+	"fmt"
+	"github.com/Mas0nShi/MHttp"
+	"sync"
 )
 
 func main() {
+	gp := sync.WaitGroup{}
+
 	headers := map[string]string{
 		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64",
 	}
 
 	for i := 0; i < 100; i++ {
+		gp.Add(1)
 		go func() {
 			http := new(MHttp.MHttp)
 			url := "http://127.0.0.1/test.php"
@@ -20,9 +23,10 @@ func main() {
 			//http.SetProxy("118.251.115.55:11405")
 			http.SetCookie("ASP.NET_SessionId", "21231313123")
 			http.Send("StudentId=12355566&Name=cxx&acadYears=2020-2021&team=2&type=1")
-			console.Log(http.GetResponseText())
+			fmt.Println(http.GetResponseText())
+			gp.Done()
 		}()
 
 	}
-	time.Sleep(10 * time.Second)
+	gp.Wait()
 }
